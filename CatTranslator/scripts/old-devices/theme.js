59f7e8ec1d0ae88bf04cd25d26d35ac026437d44
Raphaël <raphitou.montes@gmail.com>
2019-07-30 21:35:46 +0200

    Android <7.0 support for CatTranslator

---
+++ CatTranslator/scripts/old-devices/theme.js
@@ -0,0 +1,17 @@
+const theme = document.querySelector("#theme");
+const button = document.querySelector("#themeButton");
+
+var current = getCookie('theme') ? getCookie('theme') : 'light';
+theme.href = './styles/' + current + '.css';
+
+function changeTheme() {
+    if (current === "dark") {
+        current = 'light';
+        button.innerHTML = 'Dark Theme';
+    } else {
+        current = 'dark';
+        button.innerHTML = 'Light Theme';
+    }
+    setCookie('theme', current, 365)
+    theme.href = './styles/' + current + '.css';
+}
